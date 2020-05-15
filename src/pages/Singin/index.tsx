bb35d195452561dacb2208912b3cbbbd708d18d8
brenokf <breno-kf@hotmail.com>
2020-05-15 03:42:19 -0400

    addition of field logo

--- src/pages/Singin/index.tsx
+++ src/pages/Singin/index.tsx
@@ -1,38 +1,50 @@
 import React, { useState, FormEvent } from 'react';
 
 import logoside from '../../assets/sidelogo.svg';
-import { Title, Form1,Form2,Button,Layout,Side,Nconta,Fpassword} from './styles';
-const  Singin: React.FC = () => {
-  return(
+import logotimecode from '../../assets/LogoTimeCode.svg';
+import {
+  Title,
+  Form1,
+  Form2,
+  Button,
+  Layout,
+  Side,
+  Nconta,
+  Fpassword,
+  Logo,
+} from './styles';
+const Singin: React.FC = () => {
+  const [inputError, setInputError] = useState('');
+  return (
     <>
-    <Layout/>
-    <Side>
-    <img src={logoside} alt="logo side"/>
-    </Side>
-    <Title> LOGIN </Title>
-    <Form1>
+      <Layout />
+      <Side>
+        <img src={logoside} alt="logo side" />
+      </Side>
+      <Logo>
+        <img src={logotimecode}  alt="logo time code"/>
+      </Logo>
 
-      <input type ="text" id="email" placeholder=" email address"/>
-    </Form1>
-    <Form2>
-      <input type ="password" id="password" placeholder=" your password"/>
-    </Form2>
+      <Title> LOGIN </Title>
+      <Form1>
+        <input type="text" id="email" placeholder  ="Enter Email" />
+      </Form1>
+      <Form2>
+        <input type="password" id="password" placeholder="Enter Password" />
+      </Form2>
 
-    <Button>
-      <button type="submit"> Login </button>
-    </Button>
+      <Button>
+        <button type="submit"> Login </button>
+      </Button>
 
-    <Nconta>
-    <a href="register">No account</a>
-    </Nconta>
+      <Nconta>
+        <a href="register">No account</a>
+      </Nconta>
 
-    <Fpassword>
-    <a href="rememberpas">Forgot Password</a>
-    </Fpassword>
-
+      <Fpassword>
+        <a href="rememberpas">Forgot Password</a>
+      </Fpassword>
     </>
-
   );
-
-}
+};
 export default Singin;
